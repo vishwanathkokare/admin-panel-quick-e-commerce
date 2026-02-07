0221ebb7fb3a5f635dd747b805d835647b9b8b40
Vishwanath kokare <61642625+vishwanathkokare@users.noreply.github.com>
2026-02-07 10:41:57 +0000

    fetch to import

--- app/(home)/orders/page.tsx
+++ app/(home)/orders/page.tsx
@@ -2,13 +2,9 @@ import OrdersDatatable from "@/components/datatable-orders"
 import { Card } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
+import ordersData from "@/app/api/orders/db"
 
-const Orders = async () => {
-  const ordersResponse =  await fetch('http://127.0.0.1:3000/api/orders',{
-    method: 'GET'
-  })
-  const ordersData = await ordersResponse.json();
-
+const Orders = () => {
   return (
     <div className="p-5 w-full">
      <div className="flex items-center gap-4 mb-8">

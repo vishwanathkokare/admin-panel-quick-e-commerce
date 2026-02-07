86b128efc57065577704010afeaf2dae328b7dc6
Vishwanath kokare <61642625+vishwanathkokare@users.noreply.github.com>
2026-02-07 10:48:24 +0000

    workable with netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Netlify-compatible configuration */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
